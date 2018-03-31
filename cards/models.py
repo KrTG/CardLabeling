00a98d54d2206780b5ee76fc458ce6c74b68232b
= <kristoph321@gmail.com>
2018-03-31 12:05:29 +0200

    bunch of stuff

--- cards/models.py
+++ cards/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Card(models.Model):
+    color = models.CharField(max_length=20, default="")
+    rank = models.IntegerField(max_length=1, default="")
+
+    def __str__(self):
+        return "{} of {}".format(self.rank, self.color)        
+        
--- cards/views.py
+++ cards/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
+from django.http import Http404
 
-def index(request):
-    return HttpResponse("Hello, world.")
+from .models import Card
+
+def index(request, card_id):
+    try:
+        card = Card.objects.get(pk=card_id)
+    except Card.DoesNotExist:
+        raise Http404("Card does not exist")
+    
+    path = 'training_data/' + str(card_id) + '.png'
+    return render(request, 'cards/card.html',
+     {'path': path, 'card_id': card_id})
 
     
