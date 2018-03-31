1c7174d63abd0b8889624d88de16ec6ca652a00a
= <kristoph321@gmail.com>
2018-03-31 14:15:41 +0200

    idk

--- cards/models.py
+++ cards/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Card(models.Model):
     color = models.CharField(max_length=20, default="")
-    rank = models.IntegerField(max_length=1, default="")
+    rank = models.CharField(max_length=1, default="")
 
     def __str__(self):
         return "{} of {}".format(self.rank, self.color)        
