74e7c389f96a8d711729c161544a4b76882eb7c9
= <kristoph321@gmail.com>
2018-04-01 14:47:47 +0200

    bug

--- cards/helpers.py
+++ cards/helpers.py
@@ -3,7 +3,7 @@ from .models import Card
 def fetch_unidentified():
     cards = Card.objects.filter(color='', rank='')
     if (cards):
-        return cards[0].id
+        return cards[0].num
     else:
         return None
 
