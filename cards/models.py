b9edd29feadb8195e6e4a74f02789cf3ef0609b2
= <kristoph321@gmail.com>
2018-04-02 23:00:34 +0200

    baza

--- cards/models.py
+++ cards/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Card(models.Model):
     num = models.IntegerField()
     color = models.CharField(max_length=20, default="", blank=True)
-    rank = models.CharField(max_length=1, default="", blank=True)
+    rank = models.CharField(max_length=10, default="", blank=True)
 
     def __str__(self):
         return "Card nr. {}: {}, {}".format(self.num, self.rank, self.color)        
